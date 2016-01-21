6e4f1c383400eff7305d3cecce42d7c39fd41838
arookas <arookas@outlook.com>
2016-01-21 03:13:52 -0500

    Added premake5 script

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "miditobms"
+	configurations { "Debug", "Release" }
+	targetdir "bin/%{cfg.buildcfg}"
+	libdirs { "lib" }
+	
+	startproject "frontend"
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+	
+	filter "configurations:Release"
+		defines { "RELEASE" }
+		optimize "On"
+	
+	project "miditobms"
+		kind "SharedLib"
+		language "C#"
+		namespace "arookas"
+		location "miditobms"
+		
+		links { "System", "arookas", }
+		
+		files {
+			"miditobms/**.cs",
+		}
+		
+		excludes {
+			"miditobms/bin/**",
+			"miditobms/obj/**",
+		}
+		
